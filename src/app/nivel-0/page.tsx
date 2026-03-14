064bed25f77843d54198ed274e1a7ac65bbc2d77
danilo.volles <danilo.volles@outlook.com>
2026-03-14 10:04:45 -0300

    function vs arrow-function

---
+++ src/app/nivel-0/page.tsx
@@ -0,0 +1,13 @@
+// FUNCTION
+// export default function Page() {
+//   return <div>Page</div>;
+// }
+
+// ARROW-FUNCTION
+const Page2 = () => {
+  return <div>Page</div>;
+}
+
+const Page3 = () => <div>Page</div>
+
+export default Page2;
