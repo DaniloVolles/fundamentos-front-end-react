6eae49c1540865dfbe4c9d6fb60ca84a4b82541f
danilo.volles <danilo.volles@outlook.com>
2026-03-15 11:21:30 -0300

    imagens

--- src/app/nivel-0/page.tsx
+++ src/app/nivel-0/page.tsx
@@ -1,10 +1,13 @@
 import {MeuNome} from "@/components/MeuNome";
 import {Hobbies} from "@/components/Hobbies";
+import {Imagem} from "@/components/Imagem";
 
 const Page = () => {
   return <div className="p-7">
     <MeuNome name={"Danilo"} birthdate={new Date(2000, 0, 1)}/>
     <Hobbies />
+    Gosto de:
+    <Imagem />
   </div>
 }
 
