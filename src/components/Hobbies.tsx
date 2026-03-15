4ef71198e816714029c3904ba04fddfa7ee7ff11
danilo.volles <danilo.volles@outlook.com>
2026-03-15 11:13:47 -0300

    arrays e objetos 4

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -1,12 +1,24 @@
 const hobbies: string[] = ['Jogos', 'Idiomas', 'Comer']
 
-export const Hobbies= () => (
-    <>
-        <p>Meus Hobbies são:</p>
-        <ul className="list-disc pl-8">
-            {hobbies.map((hobby, i) => (
-                <li key={`hobby-${i}`}>{hobby}</li>
-            ))}
-        </ul>
-    </>
-)
+export const Hobbies = () => {
+
+    const [hobbyPreferido, ...outrosHobbies] = hobbies;
+    const novosHobbies = [ "Namorar" ];
+
+    const meusHobbies = [
+        ...outrosHobbies,
+        novosHobbies
+    ]
+
+    return (
+        <>
+            <p>Meus Hobbies são:</p>
+            <ul className="list-disc pl-8">
+                <li className="font-bold">{hobbyPreferido}</li>
+                {meusHobbies.map((meuHobbie, i) => (
+                    <li key={`hobby-${i}`}>{meuHobbie}</li>
+                ))}
+            </ul>
+        </>
+    )
+}
