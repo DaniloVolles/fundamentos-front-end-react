7f9a50e88f96c90eabbd13ada5f5725b5baa1c69
danilo.volles <danilo.volles@outlook.com>
2026-03-16 19:02:38 -0300

    organização de estilos

--- src/app/nivel-0/[name]/page.tsx
+++ src/app/nivel-0/[name]/page.tsx
@@ -12,13 +12,14 @@ const Page = async ({params}: PageProps) => {
     const {name} = await params;
 
     return (
-        <div className="p-7">
+        <div className="p-4 grid gap-y-5">
 
-            
             <MeuNome name={name} birthdate={new Date(2000, 0, 1)}/>
             <Hobbies/>
-            Gosto de:
-            <Imagem/>
+            <div>
+                <p>Gosto de:</p>
+                <Imagem/>
+            </div>
             <Link className="underline" href="/nivel-0">
                 Voltar
             </Link>
